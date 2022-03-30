562dded9f7f0594d40368c0604af80714d2203e4
Rangel <rangelplachkov1@gmail.com>
2022-03-30 13:15:00 +0300

    added palindrome function
    also polished the code

--- OOP/Homework/Homework1.cpp
+++ OOP/Homework/Homework1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+#include<cmath>
 class interval{
-    private:     //проблем ли е да го пишем въпреки че е ненужно?
+    private:    
         int A;
         int B;
         bool ValidInputCheck(){
@@ -8,23 +9,29 @@ class interval{
                 setPoints(0,0); //алтернативно може this->a = 0; this->b = 0; ако е проблем "рекурсията";
             }
         }
-        bool isItPrime(int& number)const{
-            for(int i=2; i<= number/2;i++){   //със cmath може num^(1/2)
+        bool isItPrime(int number)const{
+            int temp = sqrt(number);
+            for(int i=2; i <= temp; i++){  
                 if(number%i == 0){
                     return false;
                 }
             }
             return true;
         }
-        bool isItPalindrome(int& number){
-            /*
-            int 
-            for(){
-                if(number%10 == )
+        bool isItPalindrome(int number)const{
+            int rev=0, rem, temp=number;
+             while(temp>0){
+                rem = temp%10;
+                rev = (rev*10)+rem;
+                temp = temp/10;
             }
-            */
+            if(rev==number){
+                return true;
+            }else{
+                return false;
+            }
         }
-        bool MatchingNumbersCheck(int& number)const{
+        bool MatchingNumbersCheck(int number)const{
             bool numbers[10];
             for(int i=1;i<=10;i++){
                 numbers[i]=0;
@@ -39,18 +46,19 @@ class interval{
             }
             return true;
         }
-        bool isItPowOfTwo(int& number)const{
-            while(number>0){
-                if(number%2 == 0){
-                    number=number/2;
-                }else{
-                    return false;
-                }
+        bool isItPowOfTwo(int number)const{
+            if(number<=0){
+                return false;
             }
-            return true;
+            while(number%2 == 0){
+                number=number/2;
+            }
+            if(number == 1){
+                return true;
+            }else{ 
+                return false;
+            }
         }
-
-
 
     public:
         interval(){
@@ -76,7 +84,7 @@ class interval{
         int primaryNumbersCount()const{
             int counter = 0;
             for(int i=this->A;i<=this->B;i++){
-                if(isItPrime(i) == true){ //по оптимизирано търсене??
+                if(isItPrime(i) == true){
                     counter++;
                 }
             }
@@ -90,9 +98,11 @@ class interval{
             }
             if(startA < 0){
                 startA = 0;
-            }                   //оптимизирам ако има отрицателни числа
+            }                   
             for(int i=startA;i<=this->B;i++){
-                ////////////////////////////////////////////////
+                if(isItPalindrome(i) == true){
+                    counter++;
+                }
             }
             return counter;
 
@@ -106,10 +116,20 @@ class interval{
             }
             return counter;
         }
-        bool endsPowsofTwo(){
-            return isItPowOfTwo(this->A) && isItPowOfTwo(this->B);   //може и да порверим първо едното после другото
+        bool endsPowsofTwo()const{
+            
+            if(isItPowOfTwo(this->A) == true ){
+                if(isItPowOfTwo(this->B) == true){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+            //return isItPowOfTwo(this->A) && isItPowOfTwo(this->B);
         }
-        interval intersect(const interval& secondInterval)const{ //сечение //Как работи consт тук правилно ли го позлвам
+        interval intersect(const interval& secondInterval)const{
             interval result;
             if(this->B > secondInterval.A){    
                 result.setPoints(secondInterval.A,this->B);
@@ -149,11 +169,10 @@ class interval{
             }
             //std::cout<<Start<<End;
             result.setPoints(Start,End);
-            
             return result; 
         }
 
-        bool isSuperInterval(interval& secondInterval)const{ //сечение
+        bool isSuperInterval(const interval& secondInterval)const{ //сечение
             
             
             if(this->B >= secondInterval.B && this->A >= secondInterval.A){
@@ -175,8 +194,11 @@ int main(){
     interval t1(3,10);
     interval t2(4,14);
     interval t3;
-    
+    //std::cout<<t3.getA()<<" "<<t3.getB();
+    interval t4(2,16);
+    //std::cout<<t4.endsPowsofTwo();
     interval result = t1.intersect(t2);
+    //std::cout<<t2.PalindromeCounter();
     //std::cout<<result.getA()<<std::endl;
     //std::cout<<result.getB();
     //std::cout<<t2.isSuperInterval(result);
@@ -185,8 +207,8 @@ int main(){
     //interval resultTwo = t1.combinationInterval(t2);
     //std::cout<<resultTwo.getA()<<" ";
     //std::cout<<resultTwo.getB();
-    std::cout<<t1.combinationInterval(t2).getA()<<" ";
-    std::cout<<t1.combinationInterval(t2).getB();
+    //std::cout<<t1.combinationInterval(t2).getA()<<" ";
+    //std::cout<<t1.combinationInterval(t2).getB();
 
 
 }
