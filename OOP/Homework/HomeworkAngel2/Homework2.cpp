62f182d37c9d322069cf2c66317ab3288a7dc4ae
Rangel <rangelplachkov1@gmail.com>
2022-03-31 23:17:38 +0300

    task1 complete
    Stable

--- OOP/Homework/HomeworkAngel2/Homework2.cpp
+++ OOP/Homework/HomeworkAngel2/Homework2.cpp
@@ -26,20 +26,28 @@ class File{
     
 
 };*/
-bool viewFile(const char* source, int size){
-  //const char* source;
-  ifstream iFile(source, ios::binary);
-  if(iFile.is_open() == false){
-    return false;
-  }
-  
+unsigned getFileSize(std::fstream &iFile){          //size_t??? //тук се пробвах да го викна само с ifstream обаче не се компилира
+	unsigned currentPosition = iFile.tellg();                     //В такъв влучай просто навсякъде fstream ли въпреки че за самата функция не променям неща
+
+	iFile.seekg(0, ios::end);
+	unsigned result = iFile.tellg();
+	iFile.seekg(currentPosition);
+	return result;
+}
+void viewFile(std::fstream& iFile){  //каква е разликата дали го викам обикновенно или по референция
+
+  unsigned size = getFileSize(iFile); //не е завършено
+  //cout<<size;
+  iFile.clear();                      //сложих го докато дебъгвах още незнам как работи
+  /*
   for(int i=0;i<size;i++){
     char symbol = iFile.get();
     
       cout<<symbol<<" "; 
-  }
-  iFile.seekg(ios::beg);
+  }*/
 
+  iFile.seekg(ios::beg);
+  
   for(int i=0;i<size;i++){
     char symbol;
     iFile.read((char*)&symbol , sizeof(char));
@@ -49,17 +57,23 @@ bool viewFile(const char* source, int size){
       cout<<".";
     }
   }
-  iFile.close();
-  return true;
+  
 }
 
 int main(){
-  ofstream file("file.dat", ios::binary);
-  const char* source = "file.dat";
+  fstream file("file.dat", ios::in | ios::out | ios::binary);
+  //const char* source = "file.dat";
+  if(!file.is_open()){
+    cout<<"ne se otvarq";
+    return 0;
+  }
   int a = 999999;
   char str[20] = "efo5i124^ub";
   file.write((const char*)&str, sizeof(str));
+  //unsigned int size = getFileSize(file); 
+  //cout<<size;
+  viewFile(file);
+
   file.close();
-  viewFile(source , sizeof(str));
 
 }
