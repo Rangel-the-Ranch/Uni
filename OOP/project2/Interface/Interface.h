0eda3f55ead47f9a688342f8d046d36508f32c3b
Rangel <rangelplachkov1@gmail.com>
2022-06-19 13:47:53 +0300

    changed main

--- OOP/project2/Interface/Interface.h
+++ OOP/project2/Interface/Interface.h
@@ -15,9 +15,9 @@ class Interface{
         void close();
         void save()const;
         void saveAs(const myString& newFile);
-        void help()const;
-        bool exit();
+        
         void print()const;
+        
         void xpath(const myString& elementId , const myString& xpath);
 
     private:
@@ -26,9 +26,15 @@ class Interface{
         myString m_file;
         bool m_unsavedChanges;
 
-
-        
-
+        /*
+        void open(const myString& fileName);
+        void close();
+        void save()const;
+        void saveAs(const myString& newFile);
+        */
+        void help()const;
+        bool exit();
+        //void print()const;
         void select(const myString& elementId , const myString& nameOfAtribute)const;
         void set(const myString& elementId , const myString& nameOfAtribute, const myString& newValue);
         void children(const myString& elementId)const;
@@ -36,5 +42,5 @@ class Interface{
         void text(const myString& elementId)const;
         void deleteAtr(const myString& elementId, const myString& nameOfAtribute);
         void newChild(const myString& elementId);
-        
+        //void xpath(const myString& elementId , const myString& xpath);
 };
